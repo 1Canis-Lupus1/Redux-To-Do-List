04fcb7328fd9b4e84f86a1eee3728085c59c116c
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-10-14 17:15:13 +0530

    Initial commit with basic layout and text fields and display list added

---
+++ src/components/addTodo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function addTodo() {
+    return (
+        <>
+            <input type="text" placeholder="What Is In Your Mind?" />
+            <button>Add</button>
+        </>
+    )
+}
+
+export default addTodo;
---
+++ src/components/visiblityFilter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VisibilityFilter({ filters }) {
+    return (
+        filters.map((filter,i) => <button key={`filter-${i}`}>{filter}</button>)
+    )
+}
+
+export default VisibilityFilter;
